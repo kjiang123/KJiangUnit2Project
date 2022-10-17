bef73fd6e78e153f6571a0bd8306644905a4a388
1E10 Laptop <sample@bths.edu>
2022-10-17 12:34:14 -0400

    finish

---
+++ src/LinearEquation.java
@@ -0,0 +1,19 @@
+public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1=x1;
+        this.y1=y1;
+        this.x2=x2;
+        this.y2=y2;
+    }
+
+    public String calcSlope(){
+        if ((y2-y1)/(x2-x1) == ){
+            return y2-y1+"/"+x2-x1;
+        }else
+    }
+}
---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a coordinate point: ");
+        String point1 = scan.nextLine();
+        int comma1 = point1.indexOf(",");
+
+        String sX1 = point1.substring(1,comma1);
+        int x1 = Integer.parseInt(sX1);
+        String sY1 = point1.substring(comma1+2, point1.length()-1);
+        int y1 = Integer.parseInt(sY1);
+
+        System.out.print("Enter a coordinate point: ");
+        String point2 = scan.nextLine();
+        int comma2 = point2.indexOf(",");
+
+        String sX2 = point2.substring(1,comma1);
+        int x2 = Integer.parseInt(sX2);
+        String sY2 = point2.substring(comma1+2, point2.length()-1);
+        int y2 = Integer.parseInt(sY2);
+
+    }
+}
